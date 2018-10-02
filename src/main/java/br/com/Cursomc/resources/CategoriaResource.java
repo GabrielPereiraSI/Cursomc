63940397b13526d257d1f796652f54998ef73cf6
Gabriel Alves <gabri10_alves@hotmail.com>
2018-10-01 21:31:44 -0300

    commit teste de classe de dominio categoria

--- src/main/java/br/com/Cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/Cursomc/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package br.com.Cursomc.resources;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.Cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 		
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
+		
+		List <Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
